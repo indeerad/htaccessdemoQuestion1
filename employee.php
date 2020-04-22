c2bb93cf24269e1a66c7055b67e8a03e712d594b
indeerad <58538346+indeerad@users.noreply.github.com>
2020-04-22 10:06:45 +0530

    Chanhed the router file to direct url to a method in each file
    /employee/finish  => employee.php -> finish()
    
    /employee/display  => employee.php -> display()
    
    /project/finish  => project.php -> finish()
    
    /project/display  => project.php -> display()

---
+++ employee.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Employee</title>
+</head>
+<body>
+    <?php
+        function display(){
+            echo("<h1>Employee Display</h1>");
+        }
+
+        function finish(){
+            echo("<h1>Employee Finish</h1>");
+        }
+
+    ?>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,12 +1,33 @@
 <?php
-    $requestedURL = $_SERVER['REQUEST_URI'];           // '/htaccessdemo/employe'
+    $requestedURL = $_SERVER['REQUEST_URI'];           // '/htaccessdemo/employee/display'
     $urlParts = explode("/",$requestedURL);    // an array will be created saperated by '/'
-    $url = $urlParts[2];                            // 'employee'
 
-    if($url=='employee'){
-        require './employee.html';
+    //var_dump(sizeof($urlParts));   // length of $urlParts '4'
+
+    if(sizeof($urlParts)>=3){
+        $fileName = $urlParts[2];    // 'employee'
     }
-    if($url=='project'){
-        require './project.html';
+
+    if (sizeof($urlParts)>=4){
+        $methodName = $urlParts[3];  // 'display'
     }
+
+    switch ($fileName){
+
+        case "employee" :   require './employee.php';
+                            switch ($methodName){
+                                case "display" : display(); break;
+                                case "finish"  : finish();
+                            }
+                            break;
+
+        case "project" :   require './project.php';
+                            switch ($methodName){
+                                case "display" : display(); break;
+                                case "finish"  : finish();
+                            }
+                            break;
+
+    }
+
 ?>
